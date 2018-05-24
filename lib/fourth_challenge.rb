9bea9e62917c3e94ed09ffd81105fa8240bb6e2d
Robert Tabares <robtabares92@gmail.com>
2018-05-24 18:10:55 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -1,13 +1,13 @@
 def fourth_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {name: "Lord Montague", age: "53"},
-      :matriarch => {name: "Lady Montague", age: "54"},
+      :patriarch => {name: "Lord Montague", age: "54"},
+      :matriarch => {name: "Lady Montague", age: "53"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
         #your hashes here!
       ]
-   }, 
+   },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
@@ -18,6 +18,6 @@ def fourth_challenge
    }
 }
 
-  
+
 
 end
